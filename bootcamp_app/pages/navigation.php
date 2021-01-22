8f8acbd1da0c217ffb4ac14e547175fd4845e93a
Kristaps <viksnekristaps@gmail.com>
2021-01-22 21:53:12 +0200

    first change

--- bootcamp_app/pages/navigation.php
+++
@@ -1,6 +0,0 @@
-<div class="topnav">
-  <a href="../bootcamp_app/page/index.php">Home</a>
-  <a href="../bootcamp_app/page/contacts.php">Contacts</a>
-  <a href="../bootcamp_app/page/index.php">Login</a>
-  <a href="../bootcamp_app/page/index.php">Logout</a>
-</div>
